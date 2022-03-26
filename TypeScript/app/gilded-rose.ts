25c89dc43eb178860ef453279ec65d9c39c0e267
Sanensi <william.picard@hotmail.com>
2022-03-26 17:21:44 -0400

    Handle Sulfuras

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -36,12 +36,12 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
+    this.items.forEach((item, i) => {
+      if (item instanceof Sulfuras) return
+
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1
-          }
+          this.items[i].quality = this.items[i].quality - 1
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -60,16 +60,12 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1
-              }
+              this.items[i].quality = this.items[i].quality - 1
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
@@ -80,7 +76,7 @@ export class GildedRose {
           }
         }
       }
-    }
+    })
 
     return this.items;
   }
