7e9f2bddc426bcc6aad01224b6457fa492b1ef62
Sanensi <william.picard@hotmail.com>
2022-03-27 12:03:24 -0400

    Flatten another round of ifs

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -55,21 +55,19 @@ export class GildedRose {
         }
       }
 
-      if (item.sellIn <= 0) {
-        if (item instanceof BackstagePass) {
-          item.quality = 0
-        }
-
-        if (item instanceof AgedBrie && item.quality < 50) {
-          item.quality++
-        }
-
-        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0) {
-          item.quality--
-        }
+      if (item instanceof BackstagePass && item.sellIn <= 0) {
+        item.quality = 0
       }
 
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0) {
+      if (item instanceof AgedBrie && item.sellIn <= 0 && item.quality < 50) {
+        item.quality++
+      }
+
+      if (!(item instanceof AgedBrie) && !(item instanceof BackstagePass) && item.quality > 0) {
+        item.quality--
+      }
+
+      if (!(item instanceof AgedBrie) && !(item instanceof BackstagePass) && item.sellIn <= 0 && item.quality > 0) {
         item.quality--
       }
 
